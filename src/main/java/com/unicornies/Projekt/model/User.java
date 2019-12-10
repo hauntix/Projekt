1e21452e5e62d76d01407dd39b8e63c4eb7fc95b
ChristianNeal015 <cjdaking030@gmail.com>
2019-12-10 14:14:56 -0500

    made pojo for User

---
+++ src/main/java/com/unicornies/Projekt/model/User.java
@@ -0,0 +1,72 @@
+package com.unicornies.Projekt.model;
+
+import org.hibernate.type.descriptor.sql.VarcharTypeDescriptor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @Column(name = "userID")
+    private String userID;
+
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    public User(){
+
+    }
+    public User(String userID, String firstName, String lastName, String email) {
+        this.userID = userID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userID=" + userID +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
