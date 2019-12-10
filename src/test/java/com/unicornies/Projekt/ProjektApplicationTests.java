661b515d748a95427f8caee0e2f0f3a156b27b90
hauntix <c.estrada@email.com>
2019-12-10 13:57:12 -0500

    init commit

---
+++ src/test/java/com/unicornies/Projekt/ProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unicornies.Projekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
