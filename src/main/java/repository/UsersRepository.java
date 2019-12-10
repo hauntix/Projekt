46cd85cbab714ad491ed147d06c46e05c5542c57
Amani3D <najreed@me.com>
2019-12-10 14:40:59 -0500

    init

--- src/main/java/repository/UsersRepository.java
+++ src/main/java/repository/UsersRepository.java
@@ -1,4 +1,7 @@
 package repository;
 
-public interface UsersRepository {
+import models.Users;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UsersRepository extends CrudRepository<Users, Long> {
 }
--- src/main/java/services/UsersService.java
+++ src/main/java/services/UsersService.java
@@ -1,4 +1,17 @@
 package services;
 
+import models.Users;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import repository.UsersRepository;
+
+@Service
 public class UsersService {
+
+    @Autowired
+    private UsersRepository usersRepository;
+
+    public void createUser(Long user_id, Users users) {
+        usersRepository.save(users);
+    }
 }
