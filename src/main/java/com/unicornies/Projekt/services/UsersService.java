758aecf5efb10db6f33436869ca6d298567c3faf
Amani3D <najreed@me.com>
2019-12-11 13:46:23 -0500

    stuff and things

--- src/main/java/com/unicornies/Projekt/services/UsersService.java
+++ src/main/java/com/unicornies/Projekt/services/UsersService.java
@@ -5,15 +5,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.unicornies.Projekt.repository.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class UsersService {
 
     @Autowired
     private UsersRepository usersRepository;
 
+    public List<User> getAllUsers(User user) {
+        List<User> listOfUsers = new ArrayList<>();
+        usersRepository.findAll().forEach(listOfUsers::add);
+        return listOfUsers;
+    }
+
     public User createUser(Long userId, User user) {
         usersRepository.save(user);
         return user;
+    }
+
+    public User updateUser(User user, Long userId) {
+        return usersRepository.save(user);
     }
 
 
